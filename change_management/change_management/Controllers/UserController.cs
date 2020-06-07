b00611ae2f2e4cbaaf4c3e3c077c3999a04d2a0a
Simon <srainslie@protonmail.com>
2020-06-07 15:58:58 +0100

    Improving User management functionality. Get users view improved and add user flow improved. UserController added and used to manage all user pages.

--- change_management/change_management/Controllers/UserController.cs
+++ change_management/change_management/Controllers/UserController.cs
@@ -1,4 +1,4 @@
-﻿using System.Diagnostics;
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using change_management.Models;
 using Microsoft.Extensions.Configuration;
@@ -12,29 +12,33 @@ using System.Collections.Generic;
 
 namespace change_management.Controllers
 {
- public class HomeController : Controller
+ public class UserController : Controller
     {
         private readonly IConfiguration _configuration;
         
-        public HomeController(IConfiguration configuration){
+        public UserController(IConfiguration configuration){
             _configuration = configuration;
         }
 
-        public IActionResult Index()
+        public IActionResult Users()
+        {
+            DatabaseService DatabaseService = new DatabaseService(_configuration);
+            List<User> users = DatabaseService.databaseSelect("users");
+            ViewData["Message"] = "User management page.";
+
+            return View(users);
+        }
+
+        public IActionResult AddUser()
         {
             return View();
         }
 
-        public IActionResult Contact()
+        public IActionResult SubmitNewUser(User user)
         {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
+            DatabaseService DatabaseService = new DatabaseService(_configuration);
+            DatabaseService.databaseUserInsert(user.forename, user.surname, user.role);
+            return RedirectToAction("Users");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
